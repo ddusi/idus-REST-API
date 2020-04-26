a13bc71d6937b26942220f4a2dc544e89b9514fd
ddusi <knormla76@naver.com>
2020-04-27 08:27:16 +0900

    django에서 PUT을 사용하기 위한 middlewares 추가(request.PUT 사용가능)

--- idus/rest_api_test/views.py
+++ idus/rest_api_test/views.py
@@ -1,25 +1,77 @@
 from django.views import View
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
+from django.shortcuts import get_object_or_404
+from .models import Member
+import json
+from django.core.serializers import serialize
 
 class IndexView(View):
-    
     def get(self, request):
-        dummy_data = {
-            'name': '죠르디',
-            'type': '공룡',
-            'job': '편의점알바생',
-            'age': 5
-        }
-        return JsonResponse(dummy_data)
-    @csrf_exempt
+        data = Member.objects.all().order_by('-id')
+        res = json.loads(serialize('json', data))
+        return JsonResponse({
+            'status' : 'OK',
+            'message' : '요청에 성공 하셨습니다.',
+            'result': res
+                            
+                        })
+
     def post(self, request):
-        return HttpResponse("Post 요청을 잘받았다")
-    @csrf_exempt
+        if request.META['CONTENT_TYPE'] == "application/json":
+            request = json.loads(request.body)
+            data = Member(name = request.POST['name'],
+                            nick_name = request.POST['nick_name'],
+                            user_id = request.POST['user_id'],
+                            user_pw = request.POST['user_pw'],
+                            phone = request.POST['phone'],
+                            email = request.POST['email'],
+                            gender = request.POST['gender'])
+        else:
+            data = Member(name = request.POST['name'],
+                            nick_name = request.POST['nick_name'],
+                            user_id = request.POST['user_id'],
+                            user_pw = request.POST['user_pw'],
+                            phone = request.POST['phone'],
+                            email = request.POST['email'],
+                            gender = request.POST['gender'])
+        data.save()
+        return HttpResponse(status=200)
+
+    # def put(self, request):
+    #     request = json.loads(request.body)
+    #     id = request['id']
+    #     phone = request['phone']
+    #     data = get_object_or_404(Member, pk=id)
+    #     data.phone = phone
+    #     data.save()
+    #     return HttpResponse(status=200)
     def put(self, request):
-        return HttpResponse("Put 요청을 잘받았다")
-    @csrf_exempt
+        if request.META['CONTENT_TYPE'] == "application/json":
+            request = json.loads(request.body)
+            id = request['id']
+            phone = request['phone']
+        else:
+            id = request.PUT['id']
+            phone = request.PUT['phone']
+        data = get_object_or_404(Member, pk=id)
+        data.phone = phone
+        data.save()
+        return HttpResponse(status=200)
+
     def delete(self, request):
-        return HttpResponse("Delete 요청을 잘받았다")
+        # data = Member.objects.get(pk=id)
+        request = json.loads(request.body)
+        id = request['id']
+        data = get_object_or_404(Member, pk=id)
+        data.delete()
+        return HttpResponse(status=200)
+    
+    # def delete(self, request):
+    # if request.META['CONTENT_TYPE'] == "application/json":
+    #     request = json.loads(request.body)
+    #     id = request['id']
+    # else:
+    #     id = request.PUT['id']
+    #     data = get_object_or_404(Member, pk=id)
+    #     data.delete()
+    # return HttpResponse(status=200)
