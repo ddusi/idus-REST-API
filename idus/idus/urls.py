507b381ab862a839095fc06b072755c2a4763f82
ddusi <knormla76@naver.com>
2020-04-27 06:35:17 +0900

    django rest-api-test 성공

--- idus/idus/urls.py
+++ idus/idus/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from member import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main),
     path('signup/', views.signuppage),
-    path('member/<int:id>', views.signup)
+    path('member/<int:id>', views.signup),
+    path('rest-api-test/', include('rest_api_test.urls'))
 ]
--- idus/rest_api_test/views.py
+++ idus/rest_api_test/views.py
@@ -1,3 +1,25 @@
+from django.views import View
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+class IndexView(View):
+    
+    def get(self, request):
+        dummy_data = {
+            'name': '죠르디',
+            'type': '공룡',
+            'job': '편의점알바생',
+            'age': 5
+        }
+        return JsonResponse(dummy_data)
+    @csrf_exempt
+    def post(self, request):
+        return HttpResponse("Post 요청을 잘받았다")
+    @csrf_exempt
+    def put(self, request):
+        return HttpResponse("Put 요청을 잘받았다")
+    @csrf_exempt
+    def delete(self, request):
+        return HttpResponse("Delete 요청을 잘받았다")
